7a5be7baec4c3fde0c7755e7804e3ffd42f764d2
thdtm <thdtm3115@gmail.com>
2025-05-12 15:54:23 +0900

    Title: 배수의 개수, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250512/배수의 개수/number-of-multipliers.py
@@ -0,0 +1,13 @@
+arr = []
+count_3 = 0
+count_5 = 0
+for _ in range(10):
+    a = int(input())
+    if a % 3 == 0:
+        count_3 += 1
+    if a % 5 == 0:
+        count_5 += 1
+print(count_3,count_5,sep=' ')
+
+        
+
